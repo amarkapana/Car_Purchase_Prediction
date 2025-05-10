c2008749dbf5a2792bd5740bdbc65045153f4189
amarkapana <159643037+amarkapana@users.noreply.github.com>
2025-05-10 11:20:36 +0530

    Add files via upload

---
+++ Car_Price_Prediction/streamlit_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+# Load the pre-trained KNN model and scaler
+model = joblib.load(r'C:\Users\HP\AI & ML Projects\Car_Price_Prediction\car_purchase_predictor.pkl')
+scaler = joblib.load(r'C:\Users\HP\AI & ML Projects\Car_Price_Prediction\scaler.pkl')
+
+st.title('Car Purchase Prediction')
+
+# Input fields
+age = st.number_input('Enter Age:', min_value=18, max_value=100, value=25)
+salary = st.number_input('Enter Salary:', min_value=0, value=50000)
+
+if st.button('Predict'):
+    # Prepare the data (scaling using the loaded scaler)
+    input_data = np.array([[age, salary]])
+    input_data_scaled = scaler.transform(input_data)
+
+    prediction = model.predict(input_data_scaled)
+
+    if prediction[0] == 1:
+        st.success('Customer is interested to purchase the car 😊')
+    else:
+        st.warning('Customer is not interested to purchase a car 😔')
